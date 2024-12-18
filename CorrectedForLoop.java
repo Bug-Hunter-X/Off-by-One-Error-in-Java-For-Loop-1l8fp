e0c3c5ea8eacf55573b6514ff56f7a22b3a1bcfc
BugHunter-X <askzenai@gmail.com>
2024-12-18 11:24:06 +0100

    Add CorrectedForLoop.java file

---
+++ CorrectedForLoop.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i = 0; i <= 10; i++) {
+            x = x + i;
+            System.out.println(x);
+        }
+    }
+}
